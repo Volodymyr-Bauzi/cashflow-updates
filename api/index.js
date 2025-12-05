1fb539b68fa6cbc1f8fddcbc90dcd76fa6ceee87
Bauzi <volodiasyd@gmail.com>
2025-12-05 03:42:15 +0200

    Fix hazel-server usage

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 const hazel = require('hazel-server');
 
 module.exports = (req, res) => {
-  const { run } = hazel({
+  return hazel({
     account: 'Volodymyr-Bauzi',
     repository: 'cashflow',
     token: process.env.GITHUB_TOKEN,
     url: 'https://cashflow-updates.vercel.app'
-  });
-  
-  return run(req, res);
+  })(req, res);
 };

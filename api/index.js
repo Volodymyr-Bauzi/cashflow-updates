27e35448b4cb503702583aa0364976cd9790655a
Bauzi <volodiasyd@gmail.com>
2025-12-05 12:10:00 +0200

    Use dynamic base URL

--- api/index.js
+++ api/index.js
@@ -1,13 +1,18 @@
 const hazel = require('hazel-server');
 
 module.exports = (req, res) => {
+  const protocol = req.headers['x-forwarded-proto'] || 'http';
+  const host = req.headers.host;
+  const baseUrl = `${protocol}://${host}`;
+
   console.log('Request URL:', req.url);
+  console.log('Base URL:', baseUrl);
   console.log('Token present:', !!process.env.GITHUB_TOKEN);
   
   return hazel({
     account: 'Volodymyr-Bauzi',
     repository: 'cashflow',
     token: process.env.GITHUB_TOKEN,
-    url: 'https://cashflow-updates.vercel.app'
+    url: baseUrl
   })(req, res);
 };

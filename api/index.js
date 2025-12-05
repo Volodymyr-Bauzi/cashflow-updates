33f0e5f110a106ffb4bde47ec522978f4a29f352
Bauzi <volodiasyd@gmail.com>
2025-12-05 11:58:42 +0200

    Update vercel config and add logging

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const hazel = require('hazel-server');
 
 module.exports = (req, res) => {
+  console.log('Request URL:', req.url);
+  console.log('Token present:', !!process.env.GITHUB_TOKEN);
+  
   return hazel({
     account: 'Volodymyr-Bauzi',
     repository: 'cashflow',
